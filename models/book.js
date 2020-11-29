ab21e190e0d7c4d0080cb8315e9e5482e863cb71
Jeff T <jeff.thao92@gmail.com>
2020-11-29 16:56:58 -0600

    Adding starting files.

---
+++ models/book.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now }
+  /* description,
+  image,
+  link
+  */
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
