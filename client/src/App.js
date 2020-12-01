4de5df5c10c35a3d76430770f92b6a062fd67e82
Jeff T <jeff.thao92@gmail.com>
2020-11-30 20:20:57 -0600

    Added imports to react-router-dom, pages/Saved.js, and Jumbotron. Added Router tag and give an exact path to the home page/books search and a path to see the saved books.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
+    <Router>
     <div>
-      <Nav />
-      <Books />
+     <Nav />
+     <Jumbotron />
+     <Route exact path="/" component={Books} />
+     <Route exact path="/saved-books" component={Saved} />
     </div>
+    </Router>
   );
 }
 
