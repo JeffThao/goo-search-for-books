225b2d405d6eda694ff612b6450ada46e5d240a2
Jeff T <jeff.thao92@gmail.com>
2020-11-30 18:58:13 -0600

    Changed last P tag in jumbotron to say, Search and Save your books.

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Jumbotron({ children }) {
   return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
-      {children}
-    </div>
+    <div className="jumbotron">
+  <h1 className="display-4">Goo search for Books!</h1>
+  <p className="lead">A Google books search engine</p>
+  <hr className="my-4" />
+  <p>Search and Save your Books!</p>
+</div>
   );
 }
 
