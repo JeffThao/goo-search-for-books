697770d19f2ab5b125bdcd7a28ac3ed9d4da3bdb
Jeff T <jeff.thao92@gmail.com>
2020-11-30 18:56:32 -0600

    Added a viewBtn to the saved index.js file.

---
+++ client/src/components/ViewBtn/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function ViewBtn(props) {
+  return (
+    <button className="viewBtn btn btn-success" {...props} type="button" >
+      View
+    </button>
+  );
+}
+
+export default ViewBtn;
