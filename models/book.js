797bbcbe928fb2237f72e6ff03e1d200b7f567e0
Jeff T <jeff.thao92@gmail.com>
2020-11-30 19:02:10 -0600

    Deleted author and synopsis. Only left title as a search criteria.

--- models/book.js
+++ models/book.js
@@ -2,14 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
-  /* description,
-  image,
-  link
-  */
+  title: { type: String, required: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
