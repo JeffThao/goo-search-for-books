11ed0096dc262fae7b0918eca3ed33057b2602b5
Jeff T <jeff.thao92@gmail.com>
2020-12-01 23:30:48 -0600

    created a build file/package.

---
+++ client/build/precache-manifest.77ddc7be79e20aaa7b895c6cef033f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abefa8eef3ea6f207a15cda7473df53a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f8113fbe878e62e64c",
+    "url": "/static/css/main.86a6fd43.chunk.css"
+  },
+  {
+    "revision": "e3c9fb716dfea796af2c",
+    "url": "/static/js/2.895ebfd3.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.895ebfd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f8113fbe878e62e64c",
+    "url": "/static/js/main.4bc0f828.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
